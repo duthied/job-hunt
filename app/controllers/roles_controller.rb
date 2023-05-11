9e61a41f86db17bc1c9e177e45d66f0d3411ce84
Devlon <devlon@Devlons-MacBook-Pro.local>
2023-05-11 14:13:27 -0400

    Adds create + validation to the role model

--- app/controllers/roles_controller.rb
+++ app/controllers/roles_controller.rb
@@ -6,4 +6,23 @@ class RolesController < ApplicationController
   def show
     @role = Role.find(params[:id])
   end
+
+  def new
+      @role = Role.new
+  end
+
+  def create
+    @role = Role.new(role_params)
+
+    if @role.save
+      redirect_to @role
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+    def role_params
+      params.require(:role).permit(:title, :notes, :status, :url)
+    end
 end
--- app/models/role.rb
+++ app/models/role.rb
@@ -1,2 +1,7 @@
 class Role < ApplicationRecord
+  validates :title, presence: true
+  validates :status, presence: true
+  validates :url, presence: true
+
+  # validates :notes, presence: true, length: { minimum: 10 }
 end
