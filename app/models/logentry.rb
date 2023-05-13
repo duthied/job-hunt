bb827a9c5d80ccda9e2db20c1bd9a54030827eed
Devlon <devlon@Devlons-MacBook-Pro.local>
2023-05-13 17:04:53 -0400

    ...

--- app/models/logentry.rb
+++ app/models/logentry.rb
@@ -1,3 +1,6 @@
 class Logentry < ApplicationRecord
+  VALID_TYPES = ['Manual', 'System']
+  validates :entry_type, inclusion: { in: VALID_TYPES }
+  validates :body, presence: true
   belongs_to :role
 end
--- app/models/role.rb
+++ app/models/role.rb
@@ -1,5 +1,9 @@
 class Role < ApplicationRecord
   validates :title, presence: true
   validates :status, presence: true
+
+  VALID_STATUSES = ['Lead', 'Interviewing', 'Bailed', 'Waiting']
+  validates :status, inclusion: { in: VALID_STATUSES }
+  
   has_many  :logentries
 end
